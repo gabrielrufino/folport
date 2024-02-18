6aea8652d0cba67f8e579659173e64e2c0043f6f
Gabriel Rufino <contato@gabrielrufino.com>
2024-02-17 21:23:47 -0300

    Initial folport

---
+++ tests/index.spec.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+import { folport } from "../src"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+describe('Folport', () => {
+    it('Should import the modules', async () => {
+        const result = await folport(join(__dirname, 'folder'))
+
+        expect(result).toEqual({
+            add: expect.any(Object)
+        })
+    })
+})
