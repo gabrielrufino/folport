836039fc0d9cb0ff134ad090621878ede5f9dd81
Gabriel Rufino <contato@gabrielrufino.com>
2024-02-17 21:34:33 -0300

    Lint fix

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,17 +1,17 @@
-import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
-import { folport } from "../src"
+import { folport } from '../src';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 describe('Folport', () => {
-    it('Should import the modules', async () => {
-        const result = await folport(join(__dirname, 'folder'))
+  it('Should import the modules', async () => {
+    const result = await folport(join(__dirname, 'folder'));
 
-        expect(result).toEqual({
-            add: expect.any(Object)
-        })
-    })
-})
+    expect(result).toEqual({
+      add: expect.any(Object),
+    });
+  });
+});
