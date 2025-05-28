7f0f81a20011d8a572ca22dc50851e7b72f77967
Gabriel Rufino <contato@gabrielrufino.com>
2025-05-28 19:02:14 -0300

    refactor: folport

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import path from 'node:path';
 import camelCase from 'camelcase';
 
 function defaultLabeler(filename) {
-  return camelCase(filename.replace('.js', ''));
+  return camelCase(path.parse(filename).name);
 }
 
 export default async function folport(folder, {
   labeler = defaultLabeler,
 } = { labeler: defaultLabeler }) {
   const files = await fs.readdir(folder);
+  const modules = files.filter((file) => file.endsWith('.js'));
 
-  const imports = files.map(async (file) => {
-    const module = await import(path.join(folder, file));
+  const imports = modules.map(async (file) => {
+    const module = await import(path.resolve(folder, file));
     const moduleName = labeler(file);
     return { [moduleName]: module };
   });
